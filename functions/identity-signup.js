ac20a81cbae205e30dc311ecb2139d4e16535485
Blake Ingenthron <blakeingenthron@gmail.com>
2020-05-19 16:16:41 -0500

    Update identity-signup.js

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -1,6 +1,37 @@
+const fetch = require("node-fetch")
+
 exports.handler = async event => {
   const { user } = JSON.parse(event.body)
   console.log(JSON.stringify(user, null, 2))
+
+  const netlifyID = user.id
+
+  const stripeID = 1
+
+  const response = await fetch("https://graphql.fauna.com/graphql", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY}`,
+    },
+    body: JSON.stringify({
+      query: `
+          mutation ($netlifyID: ID! $stripeID: ID!) {
+              createUser(data: {netlifyID: $netlifyID, stripeID: $stripeID}) {
+                  netlifyID
+                  stripeID
+              }
+          }
+          `,
+      variables: {
+        netlifyID,
+        stripeID,
+      },
+    }),
+  })
+    .then(res => res.json())
+    .catch(err => console.error(err))
+
+  console.log({ response })
 
   return {
     statusCode: 200,
