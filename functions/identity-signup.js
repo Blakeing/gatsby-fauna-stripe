7aecba01b4ef1ad8db41e47703358fb14e282095
Blake Ingenthron <blakeingenthron@gmail.com>
2020-05-19 14:18:22 -0500

    Update identity-signup.js

--- functions/identity-signup.js
+++ functions/identity-signup.js
@@ -3,6 +3,7 @@ exports.handler = async event => {
   console.log(JSON.stringify(user, null, 2))
 
   return {
-    app_metadata: { roles: ["sub:free"] },
+    statusCode: 200,
+    body: JSON.stringify({ app_metadata: { roles: ["sub:free"] } }),
   }
 }
